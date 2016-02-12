5eb1bcaa80ddd6978d8507d33ee30e94b9b88672
Lovinder Pnag <lspnag@gmail.com>
2016-02-12 08:39:06 -0500

    unit test for diameter

--- wheel2_spec.rb
+++ wheel2_spec.rb
@@ -8,5 +8,14 @@ describe 'Wheel' do
     expect(subject.method(:initialize).arity).to eq 4
   end
 
+  describe '#diameter' do
+    def set_vars rim, tire, chainring=nil, cog=nil 
+      allow(subject).to receive_messages(rim: rim, tire: tire, chainring: chainring, cog: cog)
+    end
+    it 'calculates diameter correctly' do
+      set_vars(4,2)
+      expect(subject.diameter).to eq 8
+    end
+  end
 end
 
